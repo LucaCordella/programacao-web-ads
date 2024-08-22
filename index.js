a8214ff96604813b8c5748040b1865fdae433858
LucaCordella <lucacordella12@gmail.com>
2024-08-22 10:46:58 -0300

    alteração 11 (correção de js proxima aula)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let pagina = document;
+
 let botaoSubmeter = document.getElementById("botao-submeter");
 
 botaoSubmeter.addEventListener("click", submeter);
@@ -7,6 +8,7 @@ console.log(nome);
 
 
 function submeter() {
+
     let nome = document.getElementById("nome").value;
     console.log(nome);
 
@@ -18,4 +20,30 @@ function submeter() {
 
     let dataNascimento = document.getElementById("dataNascimento").value;
     console.log(dataNascimento);
+
+    if(nome == "") { // não considerar nomes com somete espaços! dica: removr espaços em branco
+        alert("Nome não deve ser vazio!")
+    }
+
+}
+
+function validaCPF(cpf) {
+    if(cpf == "") {
+        return false;
+    }
+
+    if (/[a-zA-Z]/.test(cpf)) {
+        return false;
+    }
+
+    if (/[^0-9.-]/.test(cpf)) {
+        return false;
+    }
+
+    const cpfSemPontuacao = cpf.replace(/[.-]/g, '');
+    if (cpfSemPontuacao.length !== 11 && cpf.length !== 14) {
+        return false;
+    }
+
+    return true;
 }
